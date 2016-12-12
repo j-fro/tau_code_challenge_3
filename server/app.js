8a5f52a4cdbddbc94e77558e8c8c46f29e28d2d4
Jacob Froman <jacob.h.froman@gmail.com>
2016-12-12 15:00:35 -0600

    remembered to commit styling

--- server/app.js
+++ server/app.js
@@ -1,64 +1,69 @@
-var express = require( 'express' );
+var express = require('express');
 var app = express();
-var path = require( 'path' );
-var bodyParser = require( 'body-parser' );
+var path = require('path');
+var bodyParser = require('body-parser');
 // create 'urlEncodedParser' in case we want to inject it for post calls:
-var urlEncodedParser = bodyParser.urlencoded( { extended: true } );
+var urlEncodedParser = bodyParser.urlencoded({
+    extended: true
+});
 // use bodyParser.urlencoded throughout the app with this:
-app.use( bodyParser.urlencoded( { extended: false } ) );
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 // initial jokes provided by the client
-jokes = [
-  {
+jokes = [{
     whoseJoke: "Danny",
     jokeQuestion: "Why do scuba divers fall backwards out of boats?",
     punchLine: "If they fell forwards they’d still be in the boat"
-  },
-  {
+}, {
     whoseJoke: "Luke",
     jokeQuestion: "Twofish are in a tank. What did one fish say to the other?",
     punchLine: "Do you know how to drive this thing?"
-  },
-  {
+}, {
     whoseJoke: "Millie",
     jokeQuestion: "What do you call a pile of cats?",
     punchLine: "A meowntain!"
-  },
-  {
+}, {
     whoseJoke: "dEv",
     jokeQuestion: "Why should you not play cards in the forest?",
     punchLine: "Too many Cheetahs"
-  }
-];
+}];
 
 app.post('/', function(req, res) {
     console.log('posting jokes', req.body);
     jokes.push(req.body);
-    res.send({ jokes: jokes });
+    res.send({
+        jokes: jokes
+    });
 });
 
 app.post('/removeJoke', function(req, res) {
     console.log('removeing joke', req.body);
     jokes.splice(req.body.index, 1);
-    res.send({ jokes: jokes });
+    res.send({
+        jokes: jokes
+    });
 });
 
 app.get('/jokes', function(req, res) {
     console.log('getting jokes');
-    res.send({ jokes: jokes });
+    res.send({
+        jokes: jokes
+    });
 });
 
-app.get( '/', function( req, res ){
-  // base url
-  console.log( 'base url hit' );
-  res.sendFile( path.resolve( 'views/index.html' ) );
+app.get('/', function(req, res) {
+    // base url
+    console.log('base url hit');
+    res.sendFile(path.resolve('views/index.html'));
 }); // end base url
 
 // spin up server
-app.listen( 3333, function(){
-  console.log( 'server up on 3333' );
+app.listen(3333, function() {
+    console.log('server up on 3333');
 }); // end spin up server
 
 
 
-app.use( express.static( 'public' ) );
+app.use(express.static('public'));
